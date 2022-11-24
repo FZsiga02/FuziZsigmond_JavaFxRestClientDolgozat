362b6da52b1719986043f0b1399026b8277f090a
FZsiga02 <fzsigmond02@gmail.com>
2022-11-24 08:32:39 +0100

    list-animal-view

--- src/main/java/hu/petrik/fuzizsigmond_javafxrestclientdolgozat/Controller.java
+++
@@ -1,8 +0,0 @@
-package hu.petrik.fuzizsigmond_javafxrestclientdolgozat;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
-public class Controller {
-
-}
---
+++ src/main/java/hu/petrik/fuzizsigmond_javafxrestclientdolgozat/ListAnimalController.java
@@ -0,0 +1,17 @@
+package hu.petrik.fuzizsigmond_javafxrestclientdolgozat;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ListAnimalController {
+
+    public void insertClick(ActionEvent actionEvent) {
+    }
+
+    public void updateClick(ActionEvent actionEvent) {
+    }
+
+    public void deleteClick(ActionEvent actionEvent) {
+    }
+}
