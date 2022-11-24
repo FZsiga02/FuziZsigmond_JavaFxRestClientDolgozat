ceb0f2e1be4f636c7257edb96fc5a4cd26808971
FZsiga02 <fzsigmond02@gmail.com>
2022-11-24 08:26:53 +0100

    App.java

--- src/main/java/hu/petrik/fuzizsigmond_javafxrestclientdolgozat/App.java
+++ src/main/java/hu/petrik/fuzizsigmond_javafxrestclientdolgozat/App.java
@@ -8,10 +8,13 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class App extends Application {
+
+    public static final String BASE_URL = "https://retoolapi.dev/RdaKaD/pets";
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("list-animal-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Animals");
         stage.setScene(scene);
         stage.show();
