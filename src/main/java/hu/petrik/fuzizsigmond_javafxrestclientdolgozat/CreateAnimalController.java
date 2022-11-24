4328633e950c624672fb4324c639b112650eb1ac
FZsiga02 <fzsigmond02@gmail.com>
2022-11-24 10:02:11 +0100

    Insert

--- src/main/java/hu/petrik/fuzizsigmond_javafxrestclientdolgozat/CreateAnimalController.java
+++ src/main/java/hu/petrik/fuzizsigmond_javafxrestclientdolgozat/CreateAnimalController.java
@@ -1,9 +1,54 @@
 package hu.petrik.fuzizsigmond_javafxrestclientdolgozat;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
 
-public class CreateAnimalController {
+import java.io.IOException;
+
+public class CreateAnimalController extends Controller{
+
+    @FXML
+    private TextField nameField;
+    @FXML
+    private Spinner<Integer> ageField;
+    @FXML
+    private CheckBox isBird;
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    private void initialize() {
+        SpinnerValueFactory.IntegerSpinnerValueFactory valueFactory =
+                new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 200, 30);
+        ageField.setValueFactory(valueFactory);
+    }
 
     public void submitClick(ActionEvent actionEvent) {
+        String name = nameField.getText().trim();
+        int age = ageField.getValue();
+        boolean bird = isBird.isSelected();
+        if (name.isEmpty()) {
+            warning("Name is required");
+            return;
+        }
+        Animal newAnimal = new Animal(0, name, age, bird);
+        Gson converter = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+        String json = converter.toJson(newAnimal);
+        try {
+            Response response = RequestHandler.post(App.BASE_URL, json);
+            if (response.getResponseCode() == 201) {
+                warning("Pet added!");
+                nameField.setText("");
+                ageField.getValueFactory().setValue(30);
+            } else {
+                String content = response.getContent();
+                error(content);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module hu.petrik.fuzizsigmond_javafxrestclientdolgozat {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.google.gson;
+    requires jdk.internal.le;
 
 
     opens hu.petrik.fuzizsigmond_javafxrestclientdolgozat to javafx.fxml, com.google.gson;
